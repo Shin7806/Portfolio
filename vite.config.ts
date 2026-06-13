964179c6b60bdaf926fc6372a373b597d9d836b5
Shin7806 <dhruvp0269@gmail.com>
2026-06-13 21:11:35 +0530

    Fix base path for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ function figmaAssetResolver() {
 
 export default defineConfig({
   // IMPORTANT for GitHub Pages
-  base: "/Resume/",
+  base: "/",
 
   plugins: [figmaAssetResolver(), react(), tailwindcss()],
 
